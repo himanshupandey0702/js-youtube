c9e48ae40691f30278867fb7e373d9efc7ab155b
Himanshu Pandey <146207006+himanshupandey0702@users.noreply.github.com>
2024-09-14 17:52:39 +0000

    datatyepes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "pandey0702@gmail.com"
+var accountPassword = "12345"
+accountCity = "Roorkee"
+
+//accountId = 2 //not allowed
+
+accountEmail = "hp@hp.com"
+accountPassword = "2345"
+accountCity = "Gonda"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert (3+3) // we are using node js not browser
+
+// code should be readable and readability should be high
+
+let name = "Himanshu" // this datatype(variable) is string
+let age = 23 // this datatype(variable) is number
+let isLoggedIn = false // this datatype(variable) is boolean
+
+// number => 2 to power 53 (range of number)
+//bigint
+// string=> ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
