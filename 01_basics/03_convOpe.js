2c3bfaf330cb17b919803715274bb94438e5ade6
Himanshu Pandey <146207006+himanshupandey0702@users.noreply.github.com>
2024-09-18 17:00:50 +0000

    Conversion of datatypes

--- 01_basics/03_convOpe.js
+++ 01_basics/03_convOpe.js
@@ -27,3 +27,24 @@ let stringNumber = String(someNumber)
 // console.log(typeof stringNumber)
 
 // ******* Operations *******
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2/3)
+
+let str1 = "Hello"
+let str2 = "himanshu"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "2")
+
+// console.log(+true) // this is confusing code 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
