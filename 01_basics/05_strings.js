3e31e5d21ba539b0e6eec58770733ebaf4d0b2a0
Himanshu Pandey <146207006+himanshupandey0702@users.noreply.github.com>
2024-09-24 17:13:16 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "Himanshu"
+const repoCount = 4
+
+// console.log(name + repoCount + "Value") {We don't use this syntax, it is outdated insted we use `` & $}
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
