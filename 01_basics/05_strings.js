1599ac7eaf180a54f9fce4e0f76f32a2a57a3110
Himanshu Pandey <146207006+himanshupandey0702@users.noreply.github.com>
2024-10-09 06:11:00 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,4 +3,33 @@ const repoCount = 4
 
 // console.log(name + repoCount + "Value") {We don't use this syntax, it is outdated insted we use `` & $}
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('rebel-blood-com')
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('b'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   harsh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://himanshu.com/himanshu%20pandey"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('himanshu'))
+
+console.log(gameName.split('-'));
